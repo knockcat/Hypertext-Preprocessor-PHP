0b437d23e0d4edf8a007d124d6c0df0b4858421a
VICKY <vishujoshi21nov2000@gmail.com>
2023-06-03 10:27:21 +0530

    Variables in PHP

---
+++ 001variables.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+// printing syntax
+
+echo "Let's Begin <br>" ;
+
+?>
+
+<?php
+
+// Variable are Containers for storing infomation
+// Starts with a $
+
+$name = "vishal";
+$income = "400000000";
+
+echo "My name is $name and my income is $income <br>";
+
+echo "$name is a good coder <br>";
+
+echo "$name is knockcat <br>";
+
+// Variables are Case Sensitive
+
+$namE = "Vicky <br>";
+
+echo "$namE (namE) is Case Sensitive";
+?>
+
+<h2>Rules For Creating Varibles in PHP</h2>
+
+<ul>
+    <li>Starts with a $ sign.</li>
+    <li>Cannot start with a number.</li>
+    <li>Must start with a letter or underScore character.</li>
+    <li>Can only contain alphanumeric Characters and underScores.</li>
+    <li>Variable in PHP are Case Sensitive $harry , $Harry, $hARRY are all different.</li>
+    <?php  // $n = "Vicky"; echo "$n"; ?>
+</ul>
+
+</body>
+</html>
