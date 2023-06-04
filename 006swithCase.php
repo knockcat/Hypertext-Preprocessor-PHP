e1937fb2e010e65abfeb4a2a04b0e33edd456658
VICKY <vishujoshi21nov2000@gmail.com>
2023-06-04 17:55:38 +0530

    Switch Case in PHP

---
+++ 006swithCase.php
@@ -0,0 +1,21 @@
+<?php
+
+$age = 53;
+
+switch($age)
+{
+    case 12:
+        echo "you are 12 years old <br>";
+        break;
+    case 43:
+        echo "you are 43 years old <br>";
+        break;
+    case 53;
+        echo "you are 53 years old <br>";
+        break;
+    default:
+        echo "none matches <br>";
+        break;
+}
+
+?>
