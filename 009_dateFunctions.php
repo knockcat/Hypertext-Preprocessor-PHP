26b9f5c55f176ed1962f36f93e87fbbba1f3ce3f
VICKY <vishujoshi21nov2000@gmail.com>
2023-06-06 08:39:56 +0530

    Date Functions in PHP

---
+++ 009_dateFunctions.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "Data Functions in php <br>";
+
+
+$d = date("l j F Y, g:i, A");
+
+// d in place of l for date without 0
+
+echo "Todays day is $d <br>";
+
+echo date('1 jS \of F Y h:i:s A');
+
+echo "<br>";
+
+$year = date("Y");
+
+echo "@copyright $year | All rights reserved <br>";
+
+
+echo "Refer PHP documentation for more <br>";
+?>
